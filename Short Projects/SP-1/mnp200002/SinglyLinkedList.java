bc69ad18c1d630af26e056652f02f8adaacad8b2
Manav Prajapati <manavprajapati1398@gmail.com>
2023-01-25 21:57:27 -0600

    Added unzip functionality

--- Short Projects/SP-1/mnp200002/SinglyLinkedList.java
+++ Short Projects/SP-1/mnp200002/SinglyLinkedList.java
@@ -1,4 +1,4 @@
-/** @author rbk
+/* @author rbk
  *  Singly linked list: for instructional purposes only
  *  Ver 1.0: 2018/08/21
  *  Ver 2.0: 2018/08/28: modified to be able to extend to DoublyLinkedList
@@ -16,7 +16,7 @@ public class DoublyLinkedList<T> extends SinglyLinkedList<T> {
 
  */
 
-package idsa; 	// change to your netid
+package mnp200002; 	// change to your netid
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.NoSuchElementException;
@@ -31,7 +31,7 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         Entry(E x, Entry<E> nxt) {
             element = x;
             next = nxt;
-        }	
+        }
     }
 
     // Dummy header is used.  tail stores reference of tail element of list
@@ -71,7 +71,7 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 	// Remove can be called only if next has been called and the element has not been removed
 	public void remove() {
 	    if(!ready) {
-		throw new NoSuchElementException();
+			throw new NoSuchElementException();
 	    }
 	    prev.next = cursor.next;
 	    // Handle case when tail of a list is deleted
@@ -86,33 +86,58 @@ public class SinglyLinkedList<T> implements Iterable<T> {
 
     // Add new elements to the end of the list
     public void add(T x) {
-	add(new Entry<>(x, null));
+		add(new Entry<>(x, null));
     }
 
     public void add(Entry<T> ent) {
-	tail.next = ent;
-	tail = tail.next;
-	size++;
+		tail.next = ent;
+		tail = tail.next;
+		size++;
     }
 
     public void printList() {
-	System.out.print(this.size + ": ");
-	for(T item: this) {
-	    System.out.print(item + " ");
-	}
+		System.out.print(this.size + ": ");
+		for(T item: this) {
+			System.out.print(item + " ");
+		}
 
-	System.out.println();
+		System.out.println();
     }
 
     // Rearrange the elements of the list by linking the elements at even index
     // followed by the elements at odd index. Implement by rearranging pointers
     // of existing elements without allocating any new elements.
     public void unzip() {
-	if(size < 3) {  // Too few elements.  No change.
-	    return;
-	}
-	// write the invariant
-	
+		// Unzip only if size is greater than or equal to 3
+		if(size >= 3) {
+			Entry<T> odd = head.next;
+			Entry<T> even = head.next.next;
+			Entry<T> oddFirst = odd;
+			Entry<T> evenFirst = even;
+			
+			while(true) {
+				odd.next = even.next;
+				odd = even.next;
+
+				if(odd.next == null) {
+					even.next = oddFirst;
+					break;
+				}
+
+				even.next = odd.next;
+				even = odd.next;
+
+				if(even.next == null) {
+					even.next = oddFirst;
+					odd.next = null;
+					break;
+				}
+			}
+
+			head.next = evenFirst;
+			tail = odd;
+			return;
+		}
     }
 
     public static void main(String[] args) throws NoSuchElementException {
@@ -127,34 +152,34 @@ public class SinglyLinkedList<T> implements Iterable<T> {
         }
         lst.printList();
 
-	Iterator<Integer> it = lst.iterator();
-	Scanner in = new Scanner(System.in);
-	whileloop:
-	while(in.hasNext()) {
-	    int com = in.nextInt();
-	    switch(com) {
-	    case 1:  // Move to next element and print it
-		if (it.hasNext()) {
-		    System.out.println(it.next());
-		} else {
-		    break whileloop;
+		Iterator<Integer> it = lst.iterator();
+		Scanner in = new Scanner(System.in);
+		whileloop:
+		while(in.hasNext()) {
+			int com = in.nextInt();
+			switch(com) {
+			case 1:  // Move to next element and print it
+			if (it.hasNext()) {
+				System.out.println(it.next());
+			} else {
+				break whileloop;
+			}
+			break;
+			case 2:  // Remove element
+			it.remove();
+			lst.printList();
+			break;
+			default:  // Exit loop
+			break whileloop;
+			}
 		}
-		break;
-	    case 2:  // Remove element
-		it.remove();
 		lst.printList();
-		break;
-	    default:  // Exit loop
-		break whileloop;
-	    }
-	}
-	lst.printList();
-	lst.unzip();
-    lst.printList();
-	lst.removeFirst();
-	lst.remove(9);
-	lst.addFirst(2);
-    lst.printList();
+		lst.unzip();
+		lst.printList();
+		// lst.removeFirst();
+		// lst.remove(9);
+		// lst.addFirst(2);
+		// lst.printList();
 
     }
 }
